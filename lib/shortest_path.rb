38e0d47b3d66b6f169e4c40f51038734930bf1f8
Todd Huss <thuss@gabrito.com>
2009-11-01 21:38:00 -0800

    Cleaned up the code and added some tests

--- lib/shortest_path.rb
+++ lib/shortest_path.rb
@@ -1,35 +1,36 @@
 module ShortestPath
   
   NODES = 0..1 # Nodes are in positions 0 and 1 ['A', 'B', 20]
-  LENGTH = 2   # Lenght is in position 2 
+  LENGTH = 2   # Length is in position 2 
   INFINITY = 1.0/0 # http://weblog.jamisbuck.org/2007/2/7/infinity
   
-  # Adds distance method to Array to compute the distance of the current path 
+  # Adds distance method to compute the distance of the current path 
   class Path < Array
     def distance
-      (size > 0)? inject(0) { | sum, edge | sum += edge[LENGTH] } : INFINITY
+      (any?)? inject(0) { | sum, edge | sum += edge[LENGTH] } : INFINITY
     end
   end
-  
-  # Takes a start node ('A'), end node ('B'), and array of edges ([['A','B',10], ['A','B',20]]).
-  # Returns edges representing the shortest path ([['A','B',10]]) using Dijkstra's algorithm
+
+  # Returns the unused edges between start_node and end_node
+  def unused_paths(start_node, end_node, all_edges)
+    all_edges - shortest_path(start_node, end_node, all_edges)
+  end
+
+  # Returns the shortest path between start_node and end_node
   def shortest_path(start_node, end_node, all_edges)
     adjacent_edges = all_edges.select{ | edge | edge[NODES].include?(start_node) }
     remaining_edges = all_edges - adjacent_edges
-    shortest_path = nil
-    
-    # Iterate over the adjacent edges and recursively compute optimal path ahead
+    shortest_path = Path.new
     adjacent_edges.each do | edge |
       path = Path.new [edge]
-      neighbor_node = (edge[NODES] - [start_node])[0] # ['A', 'B'] - ['A'] => 'B'
+      neighbor_node = (edge[NODES] - [start_node])[0] # ['A', 'B'] - ['A'] => ['B']
       unless neighbor_node == end_node
-        path_ahead = shortest_path(neighbor_node, end_node, remaining_edges) # Recurse ahead
-        (path_ahead.nil?)? path = nil : path.concat(path_ahead)
+        path_ahead = shortest_path(neighbor_node, end_node, remaining_edges)
+        (path_ahead.empty?)? path.clear : path.concat(path_ahead)
       end      
-      shortest_path = path if shortest_path.nil? || !path.nil? && path.distance < shortest_path.distance
+      shortest_path = path if path.distance < shortest_path.distance
     end
-    
     shortest_path
   end
-  
+      
 end
